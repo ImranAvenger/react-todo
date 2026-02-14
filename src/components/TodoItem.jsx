7e76f8d91438b954cd8a7a37e6a4f0af7ca7cdc7
Al Imran <max.imran2001@gmail.com>
2026-02-15 00:22:28 +0600

    feat: update TodoItem component with improved layout and delete icon

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,11 @@
+import { MdOutlineDelete } from "react-icons/md";
+
+
 export default function TodoItem({ item, deleteSpecificTask }) {
   return (
     <div className="flex justify-center bg-white/40 p-3 rounded-lg text-white">
-      <li className="list-none w-2/3">{item.text}</li>
-      <button className="w-1/3 cursor-pointer" onClick={() => deleteSpecificTask(item.id)}>Delete</button>
+      <li className="list-none w-7/8">{item.text}</li>
+      <button className="w-1/8 cursor-pointer flex items-center justify-center" onClick={() => deleteSpecificTask(item.id)}><MdOutlineDelete size={24} /></button>
     </div>
   );
 }
